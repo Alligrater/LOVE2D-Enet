a359e6d1416be9a9497715dc8b5c5532f694cea1
Alligrater <AtXiaoLi@outlook.com>
2020-08-20 00:27:01 -0400

    Workaround Compatibility / Font Change

--- Client/main.lua
+++ Client/main.lua
@@ -3,8 +3,13 @@
 
 local Networker
 local text = ""
-local font
+local font, tnr, ss
 local utf8
+local canvas
+--local isTextEditing = "direct" --editing, committed, direct
+--local textInput = ""'
+local isIMEOpen = false
+local inputStream = 0
 
 local queryMode = "server"
 
@@ -13,26 +18,45 @@ function love.load()
     love.window.setTitle("Networker: Client")
     Networker = require("networker")
     utf8 = require('utf8')
-    font = love.graphics.newFont("resource/zpix.ttf", 12)
-    love.graphics.setFont(font)
+    tnr = love.graphics.newFont("resource/times.ttf", 12, "mono")
+    ss = love.graphics.newFont("resource/simsun.ttc", 12)
+    tnr:setFilter("nearest","nearest")
+    ss:setFilter("nearest","nearest")
+    tnr:setFallbacks(ss)
+    love.graphics.setFont(tnr)
+    love.graphics.setDefaultFilter("nearest", "nearest")
     love.keyboard.setKeyRepeat(true)
+    canvas = love.graphics.newCanvas(400,300)
 end
 
 function love.textinput(t)
+    --if(isTextEditing == "editing") then
+    --end
+    isIMEOpen = false
+    inputStream = 0
     text = text .. t
 end
 
 function love.update(dt)
     Networker.update(dt)
+    local _, count = Networker:getChatHistory():gsub('\n', '\n')
+    love.keyboard.setTextInput( true, text:len(), (count+2) * 24, 1, 1 )
 end
 
 -- Bascailly prints the chat log, a caret and whatever you are typing next to it.
 function love.draw()
     --First, concat everything.
-    love.graphics.print(Networker.getChatHistory() .. "\r\n>" .. text, 4, 4)
+    love.graphics.setCanvas(canvas)
+    love.graphics.clear()
+        love.graphics.print(Networker.getChatHistory() .. "\r\n>" .. text .. tostring(isIMEOpen), 4, 4)
+    love.graphics.setColor(1, 1, 1, 1.0)
+    love.graphics.setCanvas()
+    love.graphics.draw(canvas,0,0, 0,2,2)
+    --love.keyboard.setInput
 end
 
 function love.keypressed(key)
+
     if(key == "return") then --If you press enter, its treated as sending the message.
         --First check whether text is empty:
         if(isBlank(text)) then return end -- Just don't do anything
@@ -59,9 +83,20 @@ function love.keypressed(key)
 
 
     elseif(key == "backspace") then
-        if(text == "" or text == nil) then text = "" return
-        else text = sub(text,1,-2) end
+        if(not isIMEOpen) then
+            if(text == "" or text == nil) then text = "" return
+            else text = sub(text,1,-2) end
+        end
+        inputStream = inputStream - 1
+        if(inputStream <= 0) then
+            isIMEOpen = false
+            inputStream = 0
+        end
     end
+    if(key:len() == 1) then
+        inputStream = inputStream + 1
+    end
+    print(inputStream)
 end
 
 function isBlank(s)
@@ -83,4 +118,9 @@ function sub(s,i,j)
     i=utf8.offset(s,i)
     j=utf8.offset(s,j+1)-1
     return string.sub(s,i,j)
+end
+
+--Somehow textedited is called one more time after thigns end.
+function love.textedited(txt,start,length)
+    isIMEOpen = ((txt:len() > 0) or inputStream > 0)
 end
